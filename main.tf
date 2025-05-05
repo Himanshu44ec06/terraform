bba50c4ff61116ddb91272b5f78c32ebb3abb1b3
Himanshu Parkash <himanshu.44ec06@gmail.com>
2025-05-05 07:14:23 +0000

    To create a dummy text file

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "MyFirstInstance"
-  }
+resource "local_file" "my_file" {
+  filename = "abc.txt"
+  content = "Hi , I am Don"
 }
